6c8e081702ae9a4cef5e40442b9be9e8a97da6e0
marcuslark <marcus.lark.stahlberg@iths.se>
2021-03-25 13:09:29 +0100

    added fetch function, json file and showhide script

---
+++ scripts/showhide.js
@@ -0,0 +1,8 @@
+var x = document.getElementById("output");
+function showHide() {
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
