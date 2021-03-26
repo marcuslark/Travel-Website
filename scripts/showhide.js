c7e2186daadbdfc7f875355bc94367c46d15366d
marcuslark <marcus.lark.stahlberg@iths.se>
2021-03-26 13:30:47 +0100

    Tobbe hjälpte mig

--- scripts/showhide.js
+++ scripts/showhide.js
@@ -1,8 +1,17 @@
 var x = document.getElementById("output");
-function showHide() {
+var y = document.getElementById("outputMessages");
+function showHide(button) {
+    if (button === 'messages') {
+        if (y.style.display === "none") {
+            y.style.display = "block";
+        } else {
+            y.style.display = "none";
+        }
+    } else {
     if (x.style.display === "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
     }
+ }
 }
